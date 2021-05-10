f012117767103f48f781873dbd1fc30dbb35c972
Daltonic <darlingtongospel@gmail.com>
2021-05-10 10:18:46 +0100

    Styled the sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,22 @@
 import './Sidebar.css'
+import CreateIcon from '@material-ui/icons/Create'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <h4>Hello Sidebar</h4>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>Cometchat (e)</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Daltonic
+          </h3>
         </div>
-    )
+        <CreateIcon />
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
